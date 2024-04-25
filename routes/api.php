7c9a93372bdc483f546ff00ab74da47a3a027eef
laptop-9gmejub1\trabe <trabelsik299@gmail.com>
2024-04-25 15:25:25 +0100

    update api

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,6 @@ Route::group([
     Route::post('/reset-password', [AuthController::class, 'resetPassword']);
     Route::post('/verify-reset-code', [AuthController::class, 'verifyResetCode']);
     Route::get('/profil', [AuthController::class, 'profil']);
-    Route::put('/mettre-A-Jour-Profil', [AuthController::class, 'mettreAJourProfil']);
+    Route::put('/update-profil', [AuthController::class, 'mettreAJourProfil']);
 
 });
