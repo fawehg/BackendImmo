072b71d33c37bae22e9e30609c57bd3aa31d7877
laptop-9gmejub1\trabe <trabelsik299@gmail.com>
2024-04-27 11:31:10 +0100

    Update RechercheOuvrierController.php

--- app/Http/Controllers/RechercheOuvrierController.php
+++ app/Http/Controllers/RechercheOuvrierController.php
@@ -1,8 +1,10 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Database\QueryException;
 use App\Models\User;
 
